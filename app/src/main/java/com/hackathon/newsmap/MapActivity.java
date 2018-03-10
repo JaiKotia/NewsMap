ed6a468c43a71e5b9ec04ad50c2845637e5ef82c
JaiKotia <jai2mad@gmail.com>
2018-03-10 17:11:10 +0530

    Firebase Storage

--- app/src/main/java/com/hackathon/newsmap/MapActivity.java
+++ app/src/main/java/com/hackathon/newsmap/MapActivity.java
@@ -2,6 +2,7 @@ package com.hackathon.newsmap;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentSender;
 import android.content.res.Resources;
 import android.location.Criteria;
@@ -13,6 +14,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,6 +38,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.security.Provider;
 import java.text.DateFormat;
@@ -78,6 +83,14 @@ public class MapActivity extends Activity implements OnMapReadyCallback,
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        // Create a storage reference from our app
+        StorageReference storageReference = storage.getReference();
+
 //        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 //
 //        try {
@@ -104,6 +117,15 @@ public class MapActivity extends Activity implements OnMapReadyCallback,
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+
+        Button upload = findViewById(R.id.upload);
+        upload.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent2 = new Intent(MapActivity.this, CameraActivity.class);
+                MapActivity.this.startActivity(intent2);
+
+            }});
 
     }
 
